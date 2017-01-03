70adbd77b369ebe2e4926907e5a502b9bf345727
omarkrostom <o.rostom.93@gmail.com>
2017-01-03 21:47:30 +0200

    modified manageData

--- app/Http/Controllers/manageUser.php
+++ app/Http/Controllers/manageUser.php
@@ -12,7 +12,6 @@ class manageUser extends Controller
     	$user = new Login();
     	$user->username = "Omar";
     	$user->password = "123456";
-    	$user->hash = [12,35,98,4656];
     	$user->save();
     }
 }
